cf4d010f0cd69fb8b5236a90377831e13151b255
Neil Tiffin <neil@fairwindsoft.com>
2023-04-05 10:33:22 -0500

    Make the script easier to use for testing by adding args and help. No significant functional changes.

--- local-build.sh
+++ local-build.sh
@@ -11,26 +11,45 @@ tmp_path=/tmp
 mkdir -p $dest_path
 cd $base_path
 
-# Set the stage for building the glossaries
-pdflatex master.tex
-# Build the glossaries
-makeglossaries master
-# Update the links.
-pdflatex master.tex
+if [[ $1 = '--use-pdf-latex' ]]; then
 
-# Make PDF
-# Not able to get latexmk to process glossaries correctly right now.
-# latexmk -dvi- \
-#     # -c  \
-#     -gg
-#     -interaction=nonstopmode \
-#     -pdf \
-#     -silent \
-#     # -aux-directory=${tmp_path} \
-#     # -output-directory=${base_path}
-#     master.tex
+    # Set the stage for building the glossaries
+    pdflatex -file-line-error master.tex
+    # Build the glossaries
+    makeglossaries master
+    # Update the links.
+    pdflatex -file-line-error master.tex
 
-# Make HTML
+elif [[ $1 = '--use-latex-mk' ]]; then
+
+    # Make PDF
+    # Not able to get latexmk to process glossaries correctly right now.
+    latexmk -dvi- \
+        -gg \
+        -interaction=nonstopmode \
+        -pdf \
+        -silent \
+        -pdflatex \
+        # -aux-directory=${tmp_path} \
+        # -output-directory=${base_path}
+        master.tex
+
+else
+
+    # Check for help or no args to provide help.
+    echo "Required options to $0 are one of:"
+    echo "  --help"
+    echo "  --use-pdf-latex"
+    echo "  --use-latex-mk # FIXME: Not working right now."
+    if [[ $1 != '--help' ]]; then
+        echo "One arg must be specified."
+        exit 1
+    fi
+    exit 0
+
+fi
+
+echo 'Make XML'
 latexml --inputencoding=utf-8 \
         --path=$img_path/ \
         --destination=book.xml \
@@ -40,6 +59,7 @@ latexml --inputencoding=utf-8 \
 # Found additional.css
 # at https://book.ledgersmb.org/1.3/split-book/additional.css
 
+echo 'Make Split-Book HTML'
 rm -rf $tmp_path/split-book
 mkdir -p $tmp_path/split-book
 cp additional.css $tmp_path/split-book/additional.css
@@ -51,6 +71,7 @@ latexmlpost --destination=$tmp_path/split-book/index.html \
             --sitedirectory=$tmp_path/split-book/ \
             book.xml
 
+echo 'Make Full-Book HTML'
 rm -rf $tmp_path/full-book
 mkdir -p $tmp_path/full-book
 cp additional.css $tmp_path/full-book/additional.css
@@ -60,6 +81,7 @@ latexmlpost --destination=$tmp_path/full-book/index.html \
             --sitedirectory=$tmp_path/full-book/ \
             book.xml
 
+echo 'Move tmp files to ${dest_path}'
 for i in full-book split-book ; do
   # Remove all of the existing files.
   rm -rf $dest_path/$i
@@ -70,9 +92,15 @@ done
 # move the pdf so it does not get cleaned up
 mv master.pdf ${dest_path}
 
-# Clean up the latex files.
+
+# echo 'Clean up the latex files'
 # This means everything will be rebuild from scratch next time.
-# Probably a better way to do this.
-latexmk -c
+# Probably a better way to do this like moving the aux files to /tmp.
+# latexmk -C
+
+
+echo "The full book can be found at: $dest_path/full-book/index.html"
+echo "The split book can be found at: $dest_path/split-book/index.html"
+echo "The pdf can be found at: $dest_path/master.pdf"
 
 exit 0
